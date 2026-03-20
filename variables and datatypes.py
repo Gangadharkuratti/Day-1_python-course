abb9ed1184d27bed6289084c4c27efd3a86622de
Gangadhar Kuratti <gangadharkuratti549@gmail.com>
2026-03-20 11:24:52 +0530

    Add files via upload

---
+++ variables and datatypes.py
@@ -0,0 +1,37 @@
+# program to add two numbers
+
+num1=4
+num2=6
+ans=num1 + num2
+print("answer is",num1+num2)
+
+# python program to find remainder when a number is divided by z
+
+num=10
+z=2
+remainder=num % z
+print("remainder is",num % z)
+
+# program to check the type of variable assingned using input() function
+
+course=input("enter something to know the type of input")
+print("you entered :",course)
+print("type :",type(course))
+
+# using comparison operator to findout whether 'a' a given variable is greatert than 'b' or not.taking a=34 and b=80
+
+a=34
+b=80
+print("is",a<b)
+
+# program to find average of two numbersentered by the user 
+
+num1=int(input("enter number 1 :"))
+num2=int(input("enter number 2 :"))
+average=(num1+num2)/2
+print("average is",average)
+
+# program to find the square of a number entered by the user
+
+num=int(input("enter number"))
+print("square of the number is",num**2)
