8581833961cad6d7fefc7e1112539e229b244ce3
Gangadhar Kuratti <gangadharkuratti549@gmail.com>
2026-03-23 19:37:41 +0530

    Add user input for fruit list and keep existing logic

--- lists and tuples.py
+++ lists and tuples.py
@@ -1,7 +1,14 @@
-# program to store seven fruits in a list entered by the user
+# program to store four fruits in a list entered by the user
 
-fruits=["orange","apple","banana","papaya","watermelon","chiku","green apple"]
-print(type(fruits))
+fruits=[]
+f1=input("enter the fruit name of your choice")
+fruits.append(f1)
+f2=input("enter the fruit name of your choice")
+fruits.append(f2)
+f3=input("enter the fruit name of your choice")
+fruits.append(f3)
+f4=input("enter the fruit name of your choice")
+fruits.append(f4)
 print(fruits)
 
 
