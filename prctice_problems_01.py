92cbf3957fc28c6acc9a4cde0bbbdab870d752b9
Gangadhar Kuratti <gangadharkuratti549@gmail.com>
2026-03-28 20:27:01 +0530

    Add files via upload

---
+++ prctice_problems_01.py
@@ -0,0 +1,26 @@
+# program to print the area of circle
+
+radius=float(input("enter number : "))
+a=3.14
+ans=(3.14)*(radius**2)
+print("area of circle is ",ans)
+
+# program to print the area of rectangle
+
+length=float(input("enter length of the rectangle : "))
+breadth=float(input("enter breadth of the rectangle : "))
+ans=(length)*(breadth)
+print("area of rectangle is ",ans)
+
+# program to print the area of triangle
+
+b=float(input("enter the base : "))
+h=float(input("enter the height  : "))
+ans=(1/2*b*h)
+print("area of triangle is ",ans)
+
+# program to print the area of square
+
+s=float(input("enter the length of the side : "))
+ans=(s**2)
+print("area of square is ",ans)
