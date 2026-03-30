912469ef75bc55e1b3db08372f52f712e291a5a5
Gangadhar Kuratti <gangadharkuratti549@gmail.com>
2026-03-30 20:45:31 +0530

    Add files via upload

---
+++ practice_problems_03.py
@@ -0,0 +1,22 @@
+# program to find the square of a number
+
+num=int(input("enter number : "))
+print(f"the square of the number {num} is",num**2)
+
+# program to find the cube of the number
+
+number=int(input("enter number : "))
+print(f"the cube of the number {number} is",number**3)
+
+# program to find the a average of the numbers
+
+num1=int(input("enter number : "))
+num2=int(input("enter number : "))
+average=(num1+num2)/2  # average of 2 numbers
+print(f"the average of the numbers entered is {average}")
+
+# program to covert celsius to fahrenheit
+
+temp=int(input("enter temperature (celsius) : "))
+far=(temp)*1.8+32
+print(f"the temperature {temp} degree celsius in fahrenheit is {far} fahrenheit")
